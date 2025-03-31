a0c247f1589a47d18ed9b634ad956a4ddc0fee0c
GiuseppeColucci95 <colucci539@gmail.com>
2025-03-31 12:59:10 +0200

    add layout with bootstrap classes
    -refactor: update index and App components for Post Form layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <header className="py-3 bg-warning">
+        <div className="container">
+          <h1>Post Form</h1>
+        </div>
+      </header>
+
+      <div className="container py-5">
+
+        <form>
+
+          <div className="row">
+
+            <div className="col-6">
+
+              <div class="mb-3">
+                <label for="author" class="form-label">Author</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="author"
+                  id="author"
+                  aria-describedby="authorHelper"
+                  placeholder="Type your author here..."
+                />
+                <small id="authorHelper" class="form-text text-muted">Type your author above</small>
+              </div>
+
+            </div>
+
+            <div className="col-6">
+              <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  name="title"
+                  id="title"
+                  aria-describedby="titleHelper"
+                  placeholder="Type your title here..."
+                />
+                <small id="titleHelper" class="form-text text-muted">Type your title above</small>
+              </div>
+            </div>
+
+            <div className="col-12">
+              <div class="mb-3">
+                <label for="body" class="form-label">Testo</label>
+                <textarea class="form-control" name="body" id="body" rows="5"></textarea>
+                <small id="bodyHelper" class="form-text text-muted">Type your request above</small>
+              </div>
+            </div>
+
+            <div className="col d-flex justify-content-center align-items-center gap-4">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="check" />
+                <label class="form-check-label" for="check">Public post</label>
+              </div>
+
+              <button type="submit" class="btn btn-primary">Send</button>
+
+            </div>
+
+          </div>
+
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
 
-export default App
+export default App;
